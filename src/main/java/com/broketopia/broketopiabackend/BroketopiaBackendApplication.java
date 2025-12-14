f2e60409b3efe7dcee0945eb19314be52c59a1dc
lukashanzl <lukas.hanzl@icloud.com>
2025-12-14 15:34:19 +0100

    Initial commit

---
+++ src/main/java/com/broketopia/broketopiabackend/BroketopiaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.broketopia.broketopiabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BroketopiaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BroketopiaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/broketopia/broketopiabackend/BroketopiaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.broketopia.broketopiabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BroketopiaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
